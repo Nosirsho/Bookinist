30a628ea271bb88e4e78f0213f9ff18a96d2bfb2
Nosirsho <Pamirikanec-m17@mail.ru>
2023-01-21 18:15:25 +0500

    Структура БД

---
+++ Bookinist.DAL/Entities/Base/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookinist.DAL.Entities.Base
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Bookinist.DAL/Entities/Seller.cs
@@ -0,0 +1,9 @@
+﻿using Bookinist.DAL.Entities.Base;
+
+namespace Bookinist.DAL.Entities
+{
+    public class Seller : Person
+    {
+        
+    }
+}
